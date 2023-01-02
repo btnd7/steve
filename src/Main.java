c8035b8421ed5a962c1796dcab6c6cf158e5ede8
btnd7 <gaal.botond.16@gmail.com>
2023-01-02 10:56:22 +0100

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int [] array = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+        int [] array = {55, 10, 15, 20, 25, 30, 35, 40, 45, 50, 5, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+
+        System.out.println(getMinValue(array));
+    }
+
+    public static int getMinValue(int[] numbers){
+        int minValue = numbers[0];
+        for(int i=1;i<numbers.length;i++){
+            if(numbers[i] < minValue){
+                minValue = numbers[i];
+            }
+        }
+        return minValue;
     }
 }
