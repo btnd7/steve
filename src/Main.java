456b7a58b38bdff2b03ca6cf955f06745c7366d5
btnd7 <gaal.botond.16@gmail.com>
2023-01-02 11:14:21 +0100

    v5

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int [] array = {55, 10, 15, 20, 25, 30, 35, 40, 45, 50, 5, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+        int[] array = {55, 10, 15, 20, 25, 30, 35, 40, 45, 50, 5, 60, 65, 70, 75, 80, 85, 90, 95, 100};
 
         System.out.println(getMinValue(array));
         System.out.println(evenNumbers(array));
         System.out.println(max(array));
         System.out.println(paratlan(array));
+        System.out.println(oddNumbers(array));
     }
 
-    public static int getMinValue(int[] numbers){
+    public static int getMinValue(int[] numbers) {
         int minValue = numbers[0];
-        for(int i = 1; i < numbers.length; i++){
-            if(numbers[i] < minValue){
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minValue) {
                 minValue = numbers[i];
             }
         }
         return minValue;
     }
 
-    public static int evenNumbers(int [] nums) {
+    public static int evenNumbers(int[] nums) {
         int ctr_even = 0;
 
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] % 2 == 0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
                 ctr_even++;
             }
         }
@@ -47,5 +48,16 @@ public class Main {
             }
         }
         return count;
+    }
+
+    public static int oddNumbers(int[] nums) {
+        int ctr_odd = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                ctr_odd++;
+            }
+        }
+        return ctr_odd;
     }
 }
